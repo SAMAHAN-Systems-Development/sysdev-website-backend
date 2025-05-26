f37ffbc4162653755c5a2310a3ef09c25d96f1ae
adriannebulao <amcbulao@gmail.com>
2025-05-26 23:39:21 +0800

    feat(project): define create project dto

--- src/project/dto/create-project.dto.ts
+++ src/project/dto/create-project.dto.ts
@@ -1,1 +1,57 @@
-export class CreateProjectDto {}
+import { Type } from 'class-transformer';
+import {
+  IsArray,
+  IsBoolean,
+  IsDateString,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+import { statusTagEnum, typeTagEnum } from 'drizzle/schema';
+
+class ProjectLinkDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  link: string;
+}
+
+export class CreateProjectDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  briefDesc: string;
+
+  @IsString()
+  @IsNotEmpty()
+  fullDesc: string;
+
+  @IsDateString()
+  dateLaunched: string;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => ProjectLinkDto)
+  @IsOptional()
+  links?: ProjectLinkDto[];
+
+  @IsArray()
+  @IsString({ each: true })
+  images: string[];
+
+  @IsEnum(statusTagEnum.enumValues)
+  status: (typeof statusTagEnum.enumValues)[number];
+
+  @IsEnum(typeTagEnum.enumValues)
+  type: (typeof typeTagEnum.enumValues)[number];
+
+  @IsBoolean()
+  @IsOptional()
+  featured?: boolean;
+}
