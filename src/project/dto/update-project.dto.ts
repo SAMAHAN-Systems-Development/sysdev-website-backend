715457758ffbffb447865eb885caace88689c30b
derickeroy2123 <derickeroy2123@gmail.com>
2025-05-28 23:11:54 +0800

    feat: add images property with validators

--- src/project/dto/update-project.dto.ts
+++ src/project/dto/update-project.dto.ts
@@ -1,4 +1,18 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateProjectDto } from './create-project.dto';
+import {
+  ArrayMinSize,
+  IsArray,
+  IsOptional,
+  IsString,
+  ValidateIf,
+} from 'class-validator';
 
-export class UpdateProjectDto extends PartialType(CreateProjectDto) {}
+export class UpdateProjectDto extends PartialType(CreateProjectDto) {
+  @IsOptional()
+  @ValidateIf((_, value) => Array.isArray(value))
+  @IsArray()
+  @ArrayMinSize(1)
+  @IsString({ each: true })
+  images?: string[];
+}
