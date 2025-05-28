99c4f79e72e6dcc7e346b62181e46bf27155ed52
Raffi Molero <raffimolero@gmail.com>
2025-05-29 00:16:42 +0800

    feat: add CreateMemberDto

--- src/members/dto/create-member.dto.ts
+++ src/members/dto/create-member.dto.ts
@@ -1,1 +1,33 @@
-export class CreateMemberDto {}
+import {
+  IsBoolean,
+  IsEmail,
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUrl,
+  MaxLength,
+} from 'class-validator';
+
+export class CreateMemberDto {
+  @IsInt()
+  roleId: number;
+
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(255)
+  name: string;
+
+  @IsEmail()
+  @MaxLength(255)
+  email: string;
+
+  @IsOptional()
+  @IsUrl()
+  @MaxLength(2048)
+  photo?: string;
+
+  @IsOptional()
+  @IsBoolean()
+  isVisible?: boolean;
+}
