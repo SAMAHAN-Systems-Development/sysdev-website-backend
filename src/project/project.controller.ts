79d3ea93807665810f54710a8dc92435c96ebd8a
derickeroy2123 <derickeroy2123@gmail.com>
2025-05-28 23:12:11 +0800

    refactor: add return type

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -12,6 +12,7 @@ import { ProjectService } from './project.service';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { UpdateProjectDto } from './dto/update-project.dto';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
+import type { Project } from './entities/project.entity';
 
 @Controller('projects')
 @UseGuards(JwtAuthGuard)
@@ -34,7 +35,10 @@ export class ProjectController {
   }
 
   @Put(':id')
-  update(@Param('id') id: string, @Body() updateProjectDto: UpdateProjectDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateProjectDto: UpdateProjectDto,
+  ): Promise<Project> {
     return this.projectService.update(+id, updateProjectDto);
   }
 
