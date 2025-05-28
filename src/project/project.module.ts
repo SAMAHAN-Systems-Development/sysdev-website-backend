b500f62e04ed3647ab3ae4e3ec8d90a0af35365d
derickeroy2123 <derickeroy2123@gmail.com>
2025-05-29 01:59:59 +0800

    feat: add MinioService and ConfigService to ProjectModule providers array

--- src/project/project.module.ts
+++ src/project/project.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { ProjectService } from './project.service';
 import { ProjectController } from './project.controller';
 import { DatabaseModule } from 'src/database/database.module';
+import { MinioService } from 'src/minio/minio.service';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [ProjectController],
-  providers: [ProjectService],
+  providers: [ProjectService, MinioService, ConfigService],
 })
 export class ProjectModule {}
