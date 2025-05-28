7e45ca19c6095798a40d8ab2b60adbb72f347da3
derickeroy2123 <derickeroy2123@gmail.com>
2025-05-29 01:59:33 +0800

    fix: remove all props

--- src/project/dto/update-project.dto.ts
+++ src/project/dto/update-project.dto.ts
@@ -1,18 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateProjectDto } from './create-project.dto';
-import {
-  ArrayMinSize,
-  IsArray,
-  IsOptional,
-  IsString,
-  ValidateIf,
-} from 'class-validator';
 
-export class UpdateProjectDto extends PartialType(CreateProjectDto) {
-  @IsOptional()
-  @ValidateIf((_, value) => Array.isArray(value))
-  @IsArray()
-  @ArrayMinSize(1)
-  @IsString({ each: true })
-  images?: string[];
-}
+export class UpdateProjectDto extends PartialType(CreateProjectDto) {}
