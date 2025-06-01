bf06698df017bbcc14669b93fbecc3c1d1eeb30b
hrrtjaymee <harrietregencia@gmail.com>
2025-06-01 21:08:33 +0800

    add delete member functionality

--- src/members/members.service.ts
+++ src/members/members.service.ts
@@ -1,10 +1,18 @@
-import { Inject, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpException,
+  HttpStatus,
+  Inject,
+  Injectable,
+} from '@nestjs/common';
 import { CreateMemberDto } from './dto/create-member.dto';
 import { UpdateMemberDto } from './dto/update-member.dto';
 import {
   DATABASE_CONNECTION,
   Database,
 } from 'src/database/database-connection';
+import { members } from 'drizzle/schema';
+import { eq } from 'drizzle-orm';
 
 @Injectable()
 export class MembersService {
@@ -28,7 +36,55 @@ export class MembersService {
     return `This action updates a #${id} member`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} member`;
+  async remove(id: number) {
+    try {
+      //find member
+      const member = await this.db
+        .select()
+        .from(members)
+        .where(eq(members.id, id));
+
+      //check if member exists
+      const foundMember = member[0];
+      if (!foundMember) {
+        throw new HttpException(
+          `Member with ID ${id} not found.`,
+          HttpStatus.NOT_FOUND,
+        );
+      }
+
+      //check if member is already deleted
+      if (foundMember.deletedAt) {
+        throw new HttpException(
+          `Member with ID ${id} is already deleted`,
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+
+      //change visibility of deleted member
+      const softDeletedMember = await this.db
+        .update(members)
+        .set({
+          isVisible: false,
+          deletedAt: new Date(),
+        })
+        .where(eq(members.id, id))
+        .returning();
+
+      return {
+        message: `Successfuly deleted member with ID ${id}`,
+      };
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      } else if (error instanceof BadRequestException) {
+        throw error;
+      }
+
+      throw new HttpException(
+        `Failed to delete member`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
